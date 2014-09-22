484e5e65e4b64de00d654e9ed7c741f5cff44779
zhouwei <leonzhouwei@gmail.com>
2014-09-22 14:56:48 +0800

    updated

--- 0200/src/main/java/com/example/springmvc/demo/HelloController.java
+++ 0200/src/main/java/com/example/springmvc/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.springmvc.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/service/greeting")
+@RequestMapping("/hi")
 public class HelloController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public @ResponseBody String getGreeting(@PathVariable String name) {
 		String result = "Hello " + name;
-		System.out.println("oops: " + result);
+		logger.debug("oops: " + result);
+		logger.info("oops: " + result);
+		logger.warn("oops: " + result);
 		return result;
 	}
-
+	
 }
