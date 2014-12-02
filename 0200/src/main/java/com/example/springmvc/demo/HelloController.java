b9e57315b5810bbfa777ae3ec6141b760922e2aa
wei.zhou <wei.zhou@autonavi.com>
2014-12-02 19:09:27 +0800

    add more jsps

--- 0200/src/main/java/com/example/springmvc/demo/HelloController.java
+++ 0200/src/main/java/com/example/springmvc/demo/HelloController.java
@@ -3,31 +3,20 @@ package com.example.springmvc.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/hi")
 public class HelloController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-	
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public @ResponseBody String getGreeting(@PathVariable String name) {
-		String result = "Hello " + name;
-		logger.debug("oops: " + result);
-		logger.info("oops: " + result);
-		logger.warn("oops: " + result);
-		return result;
+	private static final Logger logger = LoggerFactory
+			.getLogger(HelloController.class);
+
+	@RequestMapping("/hi")
+	public ModelAndView hi() {
+		logger.info("oops");
+		ModelAndView mav = new ModelAndView("admin/index");
+		return mav;
 	}
-	
-	public static void main(String[] args) {
-		String result = "Hello ";
-		logger.debug("oops: " + result);
-		logger.info("oops: " + result);
-		logger.warn("oops: " + result);
-	}
-	
+
 }
---
+++ 0200/src/main/webapp/WEB-INF/jsp/admin/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>hello from admin/index.jsp</h2>
+</body>
+</html>
